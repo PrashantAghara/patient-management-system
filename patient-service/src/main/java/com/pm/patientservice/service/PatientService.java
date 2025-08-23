3bdf85f41b455014d2fa094f326d22a965a9f655
Prashant Aghara <prashant.aghara2111@gmail.com>
2025-08-23 17:03:22 +0530

    Patient Service | Fixed update email bug

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -38,7 +38,7 @@ public class PatientService {
         Patient patient = patientRepository.findById(id)
                 .orElseThrow(() -> new PatientNotFoundException("Patient not found with id : " + id));
 
-        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+        if (patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)) {
             throw new EmailAlreadyExistsException(patientRequestDTO.getEmail() + " already exists");
         }
 
